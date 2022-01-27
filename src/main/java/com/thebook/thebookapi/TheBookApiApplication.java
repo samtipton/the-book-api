b4f6c1f112b65be87660deea7d34fcbed242b7ea
Sam Tipton <sam.tipton@gmail.com>
2022-01-27 08:14:32 -0600

    initial commit using spring initializr

---
+++ src/main/java/com/thebook/thebookapi/TheBookApiApplication.java
@@ -0,0 +1,13 @@
+package com.thebook.thebookapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheBookApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheBookApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thebook/thebookapi/TheBookApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thebook.thebookapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheBookApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
